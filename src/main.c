eb23f3d822a5f2929b94d744dc3ac1659b75050a
happou31 <happo31@gmail.com>
2022-12-10 23:35:15 +0900

    feat: 抽象構文木を導入
    - 掛け算、割り算を導入
    - 演算子の優先順位を表現できるように

--- src/main.c
+++ src/main.c
@@ -4,21 +4,22 @@
 #include <stdarg.h>
 #include <stdbool.h>
 
+typedef struct Token Token;
+typedef struct Node Node;
+
 typedef enum {
   TK_RESERVED, // 記号
   TK_NUM,     // 数値
   TK_EOF,     // EOF
 } TokenKind;
 
-typedef struct Token Token;
-struct Token {
-  TokenKind kind;  // トークンの型
-  Token *next;    // 次の入力トークン
-  int val;        // kind が TK_NUM の場合、その数値
-  char *str;      // トークン文字列
-};
-
-Token* token;
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // 整数
+} NodeKind;
 
 void error(char *fmt, ...);
 bool consume(char op);
@@ -28,6 +29,109 @@ Token *new_token(TokenKind kind, Token *cur, char *str);
 bool at_eof();
 Token *new_token(TokenKind kind, Token *cur, char *str);Token *tokenize(char *p);
 void error_at(char* loc, char* fmt, ...);
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+Node *primary();
+Node *mul();
+Node *expr();
+void gen(Node *node);
+
+struct Node {
+  NodeKind kind;
+  Node* lhs;
+  Node* rhs;
+  int val;
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *primary() {
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+  return new_node_num(expect_number());
+}
+
+Node *mul() {
+  Node *node = primary();
+  for (;;) {
+    if (consume('*')) {
+      node = new_node(ND_MUL, node, primary());
+    } else if (consume('/')) {
+      node = new_node(ND_DIV, node, primary());
+    } else {
+      return node;
+    }
+  }
+}
+
+Node *expr() {
+  Node *node = mul();
+  for (;;) {
+    if (consume('+')) {
+      node = new_node(ND_ADD, node, mul());
+    } else if (consume('-')) {
+      node = new_node(ND_SUB, node, mul());
+    } else {
+      return node;
+    }
+  }
+}
+
+void gen(Node *node) {
+  if (node->kind == ND_NUM) {
+    printf ("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch(node->kind) {
+    case ND_ADD:
+      printf("  add rax, rdi\n");
+      break;
+    case ND_SUB:
+      printf("  sub rax, rdi\n");
+      break;
+    case ND_MUL:
+      printf("  imul rax, rdi\n");
+      break;
+    case ND_DIV:
+      printf("  cqo\n");
+      printf("  idiv rdi\n");
+      break;
+  }
+
+  printf("  push rax\n");
+}
+
+struct Token {
+  TokenKind kind;  // トークンの型
+  Token *next;    // 次の入力トークン
+  int val;        // kind が TK_NUM の場合、その数値
+  char *str;      // トークン文字列
+};
+
+Token* token;
 
 void error(char *fmt, ...) {
   va_list ap;
@@ -87,7 +191,8 @@ Token *tokenize(char *p) {
       ++p;
       continue;
     }
-    if (*p == '+' || *p == '-') {
+
+    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
     }
@@ -98,7 +203,7 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    error_at(token->str, "トークナイズ出来ません");
+    error_at(p, "予期しない文字が現れました");
   }
 
   new_token(TK_EOF, cur, p);
@@ -127,26 +232,17 @@ int main(int argc, char** argv) {
   }
 
   user_input = argv[1];
-  token = tokenize(argv[1]);
+  token = tokenize(user_input);
+  Node *node = expr();
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  // 最初の文字は数字でなければならないのでそれをチェックする
-  printf("  mov rax, %d\n", expect_number());
+  gen(node);
 
-while(!at_eof()) {
-  if (consume('+')) {
-    printf("  add rax, %d\n", expect_number());
-    continue;
-  }
-  expect('-');
-  printf("  sub rax, %d\n", expect_number());
-}
-
+  printf("  pop rax\n");
   printf("  ret\n");
   
   return 0;
 }
-
